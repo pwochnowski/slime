6bcd453a609cda25e3a39c510fb784718f959d50
paulw <wochnowskip@gmail.com>
2026-06-04 09:16:12 +0000

    dev container

--- docker/build_dev_container.sh
+++ docker/build_dev_container.sh
@@ -14,7 +14,7 @@ docker run --rm --gpus all --privileged --ipc=host --name slime \
     -v /home/paul/repos/sglang:/sgl-workspace/sglang \
     -v /mnt/huge:/mnt/huge \
     --shm-size=16g --ulimit memlock=-1 --ulimit stack=67108864 \
-    -td slimerl/slime:latest /bin/zsh
+    -td slimerl/slime:working /bin/zsh
 
 # Install packages, build GCR, configure shell
 docker exec slime bash /root/slime/docker/init_container.sh
