fa53e6ea3b1c41f468b5bd20eeda3a916c123dcf
paulw <wochnowskip@gmail.com>
2026-06-06 11:04:10 +0000

    Account for multiple optimizer states

--- slime/backends/megatron_utils/actor.py
+++ slime/backends/megatron_utils/actor.py
@@ -161,13 +161,18 @@ class MegatronTrainRayActor(TrainRayActor):
         import gcr
         MT_OPTIM_TAG = 4
         with torch.cuda.use_mem_pool(self._mt_mem_pool), gcr.tagged(tag=MT_OPTIM_TAG):
-            inner_opt = self.optimizer.optimizer
-            for group in inner_opt.param_groups:
-                for p in group['params']:
-                    if len(inner_opt.state[p]) == 0:
-                        inner_opt.state[p]['step'] = torch.tensor(0.0)
-                        inner_opt.state[p]['exp_avg'] = torch.zeros_like(p.data)
-                        inner_opt.state[p]['exp_avg_sq'] = torch.zeros_like(p.data)
+            if hasattr(self.optimizer, 'chained_optimizers'):
+                optimizers = self.optimizer.chained_optimizers
+            else:
+                optimizers = [self.optimizer]
+            for opt in optimizers:
+                inner_opt = opt.optimizer
+                for group in inner_opt.param_groups:
+                    for p in group['params']:
+                        if len(inner_opt.state[p]) == 0:
+                            inner_opt.state[p]['step'] = torch.tensor(0.0)
+                            inner_opt.state[p]['exp_avg'] = torch.zeros_like(p.data)
+                            inner_opt.state[p]['exp_avg_sq'] = torch.zeros_like(p.data)
 
     def _get_rollout_data(self, rollout_data_ref: Box) -> RolloutBatch:
         # Fetch data through ray on CPU, not sure if this will be performance bottleneck.
